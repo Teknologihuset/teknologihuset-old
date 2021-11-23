033bde763cc63a1ab5a93a87b2c9258baeea3394
Tommy Hamarsnes <tommyhamarsnes@gmail.com>
2021-11-23 19:00:31 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+### Setting up locally
+Make sure you have node and npm installed, then run `npm install`, then npm run start
---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css';
+import Header from './components/Header';
+import Mvp from './pages/Mvp';
+
+function App() {
+  return (
+    <div className="app max-width">
+      <Header />
+      <Mvp />
+    </div>
+  );
+}
+
+export default App;
